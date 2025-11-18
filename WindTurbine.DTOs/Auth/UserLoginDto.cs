8d73ebf7dc1ccc2706568bd2654fccef832800fb
metehan görmez <gormezsmetehan@gmail.com>
2025-11-18 20:14:22 +0300

    log in- sign up pages

---
+++ WindTurbine.DTOs/Auth/UserLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WindTurbine.DTOs.Auth
+{
+    public class UserLoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ WindTurbine.DTOs/Auth/UserRegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WindTurbine.DTOs.Auth
+{
+    public class UserRegisterDto
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
